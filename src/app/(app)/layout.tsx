5a394e17fe140d733b02c8311bccb910b3e353af
krisna-dev-07 <prasadkrishna25102002@gmail.com>
2025-04-01 17:54:25 +0530

    Add  Navbar component and Message Card

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
-import { Toaster } from "@/components/ui/toaster"
+import "../globals.css";
 import Navbar from "@/components/Navbar";
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -22,11 +20,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <AuthProvider>
           <Navbar/>
           {children}
-          <Toaster />
-        </AuthProvider>
       </body>
     </html>
   );
